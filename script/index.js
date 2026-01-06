ea8e21c05561d5cf6f2ee0c1e1ea5235e3c72c3c
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-06 14:26:41 +0600

    addad same figma desigen

--- script/index.js
+++ script/index.js
@@ -28,46 +28,36 @@ function displayCategoric(categories) {
   }
 }
 
-// [{…}]
-// category_id
-// :
-// "1001"
-// description
-// :
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// :
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// :
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// :
-// "Shape of You"
-// video_id
-// :
-// "aaaa"
-
 const displayVideos = (videos) => {
   const videosContainer = document.getElementById("video-container");
   videos.forEach((video) => {
     console.log(video);
     const videoCard = document.createElement("div");
     videoCard.innerHTML = `
-    <div class="card  shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="photos" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${video.title}</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+       <div class="card  shadow-sm">
+            <figure class="relative">
+                <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="photos" />
+                <p class="absolute bottom-3 text-sm text-white bg-black rounded-md px-1 right-2">3hrs 56 min ago</p>
+            </figure>
+            <div class="flex gap-4 py-3 px-1">
+                <div>
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring-2 ring-offset-2">
+                            <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <h2 class="text-lg font-semibold">${video.title}</h2>
+                    <div class="flex gap-1 ">
+                        <p class="text-gray-500 text-sm">${video.authors[0].profile_name}</p>
+                        <img class="w-5 h-5"
+                            src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">
+                    </div>
+                    <p class="text-gray-500 text-sm">${video.others.views} views</p>
+                </div>
+            </div>
+        </div>
     
     `;
     videosContainer.append(videoCard);
