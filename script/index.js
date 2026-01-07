9a6194263c8c62481f9eef716050c00f6a77422e
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-07 23:49:44 +0600

    check profil verified or not verified if it is verified show the batch esle if not verified dont show the batch

--- script/index.js
+++ script/index.js
@@ -71,7 +71,9 @@ const displayVideos = (videos) => {
     videoCard.innerHTML = `
        <div class="card  shadow-sm">
             <figure class="relative">
-                <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="photos" />
+                <img class="w-full h-[150px] object-cover" src="${
+                  video.thumbnail
+                }" alt="photos" />
                 <p class="absolute bottom-3 text-sm text-white bg-black rounded-md px-1 right-2">3hrs 56 min ago</p>
             </figure>
             <div class="flex gap-4 py-3 px-1">
@@ -85,11 +87,19 @@ const displayVideos = (videos) => {
                 <div>
                     <h2 class="text-lg font-semibold">${video.title}</h2>
                     <div class="flex gap-1 ">
-                        <p class="text-gray-500 text-sm">${video.authors[0].profile_name}</p>
-                        <img class="w-5 h-5"
-                            src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">
+                        <p class="text-gray-500 text-sm">${
+                          video.authors[0].profile_name
+                        }</p>
+                        ${
+                          video.authors[0].verified == true
+                            ? `<img class="w-5 h-5"
+                            src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">`
+                            : ""
+                        }
                     </div>
-                    <p class="text-gray-500 text-sm">${video.others.views} views</p>
+                    <p class="text-gray-500 text-sm">${
+                      video.others.views
+                    } views</p>
                 </div>
             </div>
         </div>
