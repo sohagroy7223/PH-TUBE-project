b83f78bb4fc353577e0502f0149482af271c38f9
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-08 12:25:21 +0600

    adad full user frendly channel

---
+++ script/fetch.js
@@ -0,0 +1,3 @@
+// fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
--- script/index.js
+++ script/index.js
@@ -38,6 +38,54 @@ const loadCategoricVideos = (id) => {
     });
 };
 
+const loadVideoDetails = (video_id) => {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${video_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+// authors
+// :
+// [{…}]
+// category_id
+// :
+// "1001"
+// description
+// :
+// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// others
+// :
+// {views: '100K', posted_date: '16278'}
+// thumbnail
+// :
+// "https://i.ibb.co/L1b6xSq/shape.jpg"
+// title
+// :
+// "Shape of You"
+// video_id
+// :
+// "aaaa"
+
+const displayVideoDetails = (video) => {
+  // console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+  <div class="card bg-base-100 image-full  shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="thumbnail" />
+  </figure>
+  <div class="card-body ">
+    <h2 class="card-title">${video.title}</h2>
+    </div>
+  </div>
+</div>
+  `;
+};
+
 function displayCategoric(categories) {
   //   get the container
   const categoriesContainer = document.getElementById("category-container");
@@ -104,6 +152,9 @@ const displayVideos = (videos) => {
                     } views</p>
                 </div>
             </div>
+            <button onclick="loadVideoDetails('${
+              video.video_id
+            }')" class="btn btn-block">video details</button>
         </div>
     
     `;
