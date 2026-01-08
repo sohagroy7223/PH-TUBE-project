49396c908e23dcaf97160093583e05b1121ef1ec
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-08 18:19:53 +0600

    addad dymimack video card in this page

--- recap/fetch.js
+++ recap/fetch.js
@@ -1,16 +1,93 @@
+// btn category load by fetch here***************
+
 function loadCategoric() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategoric(data.categories));
 }
+
+// video category load by fetch here***************
+
+function loadVideosCategories() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideoCategories(data.videos));
+}
+
+// video necessary title***************
+// authors
+// :
+// [{…}]
+// category_id
+// :
+// "1001"
+// description
+// :
+// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// others
+// :
+// {views: '100K', posted_date: '16278'}
+// thumbnail
+// :
+// "https://i.ibb.co/L1b6xSq/shape.jpg"
+// title
+// :
+// "Shape of You"
+// video_id
+// :
+// "aaaa"
+// authors
+// :
+// Array(1)
+// 0
+// :
+// profile_name
+// :
+// "Olivia Mitchell"
+// profile_picture
+// :
+// "https://i.ibb.co/D9wWRM6/olivia.jpg"
+// verified
+// :
+// ""
+
+// display video categories************
+function displayVideoCategories(videos) {
+  // parent div where append the child/value*********
+  const videosContainer = document.getElementById("videos-container");
+  for (const video of videos) {
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+    <div class="card bg-base-100 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      
+    </div>
+  </div>
+</div>
+    `;
+    videosContainer.append(videoCard);
+  }
+}
+
+// btn category display****************
+
 function displayCategoric(categories) {
+  // parent div where append the child/value*********
   const categoriesContainer = document.getElementById("category-container");
 
   for (const cat of categories) {
     // console.log(cat);
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = `
-     <button class="btn btn-sm">${cat.category}</button>
+     <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
    
     `;
     categoriesContainer.append(categoryDiv);
@@ -18,3 +95,4 @@ function displayCategoric(categories) {
 }
 
 loadCategoric();
+loadVideosCategories();
--- script/index.js
+++ script/index.js
@@ -1,184 +1,184 @@
-const showLoader = () => {
-  document.getElementById("loader").classList.remove("hidden");
-  document.getElementById("video-container").classList.add("hidden");
-};
-const hideLoader = () => {
-  document.getElementById("loader").classList.add("hidden");
-  document.getElementById("video-container").classList.remove("hidden");
-};
+// const showLoader = () => {
+//   document.getElementById("loader").classList.remove("hidden");
+//   document.getElementById("video-container").classList.add("hidden");
+// };
+// const hideLoader = () => {
+//   document.getElementById("loader").classList.add("hidden");
+//   document.getElementById("video-container").classList.remove("hidden");
+// };
 
-function removeActiveClass() {
-  const activeButtons = document.getElementsByClassName("active");
-  for (const btn of activeButtons) {
-    btn.classList.remove("active");
-  }
-}
+// function removeActiveClass() {
+//   const activeButtons = document.getElementsByClassName("active");
+//   for (const btn of activeButtons) {
+//     btn.classList.remove("active");
+//   }
+// }
 
-function loadCategoric() {
-  //1. fetch the data
-  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
-    // 2. convert promise to json
-    .then((res) => res.json())
-    // sent to data displayCategoric
-    .then((data) => displayCategoric(data.categories));
-}
+// function loadCategoric() {
+//   //1. fetch the data
+//   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+//     // 2. convert promise to json
+//     .then((res) => res.json())
+//     // sent to data displayCategoric
+//     .then((data) => displayCategoric(data.categories));
+// }
 
-function loadVideos(inputText = "") {
-  showLoader();
-  fetch(
-    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${inputText}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      removeActiveClass();
-      document.getElementById("btn-all").classList.add("active");
-      displayVideos(data.videos);
-    });
-}
+// function loadVideos(inputText = "") {
+//   showLoader();
+//   fetch(
+//     `https://openapi.programming-hero.com/api/phero-tube/videos?title=${inputText}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       removeActiveClass();
+//       document.getElementById("btn-all").classList.add("active");
+//       displayVideos(data.videos);
+//     });
+// }
 
-const loadCategoricVideos = (id) => {
-  showLoader();
-  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      removeActiveClass();
-      const clickButton = document.getElementById(`btn-${id}`);
-      clickButton.classList.add("active");
-      displayVideos(data.category);
-    });
-};
+// const loadCategoricVideos = (id) => {
+//   showLoader();
+//   const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       removeActiveClass();
+//       const clickButton = document.getElementById(`btn-${id}`);
+//       clickButton.classList.add("active");
+//       displayVideos(data.category);
+//     });
+// };
 
-const loadVideoDetails = (video_id) => {
-  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${video_id}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayVideoDetails(data.video));
-};
+// const loadVideoDetails = (video_id) => {
+//   const url = `https://openapi.programming-hero.com/api/phero-tube/video/${video_id}`;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => displayVideoDetails(data.video));
+// };
 
-// authors
-// :
-// [{…}]
-// category_id
-// :
-// "1001"
-// description
-// :
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// :
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// :
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// :
-// "Shape of You"
-// video_id
-// :
-// "aaaa"
+// // authors
+// // :
+// // [{…}]
+// // category_id
+// // :
+// // "1001"
+// // description
+// // :
+// // "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// // others
+// // :
+// // {views: '100K', posted_date: '16278'}
+// // thumbnail
+// // :
+// // "https://i.ibb.co/L1b6xSq/shape.jpg"
+// // title
+// // :
+// // "Shape of You"
+// // video_id
+// // :
+// // "aaaa"
 
-const displayVideoDetails = (video) => {
-  // console.log(video);
-  document.getElementById("video_details").showModal();
-  const detailsContainer = document.getElementById("details-container");
-  detailsContainer.innerHTML = `
-  <div class="card bg-base-100 image-full  shadow-sm">
-  <figure>
-    <img
-      src="${video.thumbnail}"
-      alt="thumbnail" />
-  </figure>
-  <div class="card-body ">
-    <h2 class="card-title">${video.title}</h2>
-    </div>
-  </div>
-</div>
-  `;
-};
+// const displayVideoDetails = (video) => {
+//   // console.log(video);
+//   document.getElementById("video_details").showModal();
+//   const detailsContainer = document.getElementById("details-container");
+//   detailsContainer.innerHTML = `
+//   <div class="card bg-base-100 image-full  shadow-sm">
+//   <figure>
+//     <img
+//       src="${video.thumbnail}"
+//       alt="thumbnail" />
+//   </figure>
+//   <div class="card-body ">
+//     <h2 class="card-title">${video.title}</h2>
+//     </div>
+//   </div>
+// </div>
+//   `;
+// };
 
-function displayCategoric(categories) {
-  //   get the container
-  const categoriesContainer = document.getElementById("category-container");
-  //  loop operation on array of object
-  for (const cat of categories) {
-    // console.log(cat);
-    // create element
-    const categoriesDiv = document.createElement("div");
-    categoriesDiv.innerHTML = `
-  <button id="btn-${cat.category_id}" onclick="loadCategoricVideos(${cat.category_id})" class="btn btn-sm hover:text-white hover:bg-[#FF1F3D]">${cat.category}</button>
-  `;
-    //   append child
-    categoriesContainer.append(categoriesDiv);
-  }
-  hideLoader();
-}
+// function displayCategoric(categories) {
+//   //   get the container
+//   const categoriesContainer = document.getElementById("category-container");
+//   //  loop operation on array of object
+//   for (const cat of categories) {
+//     // console.log(cat);
+//     // create element
+//     const categoriesDiv = document.createElement("div");
+//     categoriesDiv.innerHTML = `
+//   <button id="btn-${cat.category_id}" onclick="loadCategoricVideos(${cat.category_id})" class="btn btn-sm hover:text-white hover:bg-[#FF1F3D]">${cat.category}</button>
+//   `;
+//     //   append child
+//     categoriesContainer.append(categoriesDiv);
+//   }
+//   hideLoader();
+// }
 
-const displayVideos = (videos) => {
-  const videosContainer = document.getElementById("video-container");
-  videosContainer.innerHTML = "";
+// const displayVideos = (videos) => {
+//   const videosContainer = document.getElementById("video-container");
+//   videosContainer.innerHTML = "";
 
-  if (videos.length === 0) {
-    videosContainer.innerHTML = `
-       <div class="py-20 col-span-full text-center flex flex-col items-center">
-            <img class="w-[110px]" src="./assets/Icon.png" alt="">
-            <h2 class="text-2xl font-bold ">Oops!! Sorry, There is no content here</h2>
-        </div>
-    `;
-    hideLoader();
-    return;
-  }
+//   if (videos.length === 0) {
+//     videosContainer.innerHTML = `
+//        <div class="py-20 col-span-full text-center flex flex-col items-center">
+//             <img class="w-[110px]" src="./assets/Icon.png" alt="">
+//             <h2 class="text-2xl font-bold ">Oops!! Sorry, There is no content here</h2>
+//         </div>
+//     `;
+//     hideLoader();
+//     return;
+//   }
 
-  videos.forEach((video) => {
-    const videoCard = document.createElement("div");
-    videoCard.innerHTML = `
-       <div class="card  shadow-sm">
-            <figure class="relative">
-                <img class="w-full h-[150px] object-cover" src="${
-                  video.thumbnail
-                }" alt="photos" />
-                <p class="absolute bottom-3 text-sm text-white bg-black rounded-md px-1 right-2">3hrs 56 min ago</p>
-            </figure>
-            <div class="flex gap-4 py-3 px-1">
-                <div>
-                    <div class="avatar">
-                        <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring-2 ring-offset-2">
-                            <img src="${video.authors[0].profile_picture}" />
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <h2 class="text-lg font-semibold">${video.title}</h2>
-                    <div class="flex gap-1 ">
-                        <p class="text-gray-500 text-sm">${
-                          video.authors[0].profile_name
-                        }</p>
-                        ${
-                          video.authors[0].verified == true
-                            ? `<img class="w-5 h-5"
-                            src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">`
-                            : ""
-                        }
-                    </div>
-                    <p class="text-gray-500 text-sm">${
-                      video.others.views
-                    } views</p>
-                </div>
-            </div>
-            <button onclick="loadVideoDetails('${
-              video.video_id
-            }')" class="btn btn-block">video details</button>
-        </div>
-    
-    `;
-    videosContainer.append(videoCard);
-  });
-  hideLoader();
-};
+//   videos.forEach((video) => {
+//     const videoCard = document.createElement("div");
+//     videoCard.innerHTML = `
+//        <div class="card  shadow-sm">
+//             <figure class="relative">
+//                 <img class="w-full h-[150px] object-cover" src="${
+//                   video.thumbnail
+//                 }" alt="photos" />
+//                 <p class="absolute bottom-3 text-sm text-white bg-black rounded-md px-1 right-2">3hrs 56 min ago</p>
+//             </figure>
+//             <div class="flex gap-4 py-3 px-1">
+//                 <div>
+//                     <div class="avatar">
+//                         <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring-2 ring-offset-2">
+//                             <img src="${video.authors[0].profile_picture}" />
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div>
+//                     <h2 class="text-lg font-semibold">${video.title}</h2>
+//                     <div class="flex gap-1 ">
+//                         <p class="text-gray-500 text-sm">${
+//                           video.authors[0].profile_name
+//                         }</p>
+//                         ${
+//                           video.authors[0].verified == true
+//                             ? `<img class="w-5 h-5"
+//                             src="https://img.icons8.com/?size=100&id=98A4yZTt9abw&format=png&color=000000" alt="">`
+//                             : ""
+//                         }
+//                     </div>
+//                     <p class="text-gray-500 text-sm">${
+//                       video.others.views
+//                     } views</p>
+//                 </div>
+//             </div>
+//             <button onclick="loadVideoDetails('${
+//               video.video_id
+//             }')" class="btn btn-block">video details</button>
+//         </div>
 
-document.getElementById("search-input").addEventListener("keyup", (event) => {
-  const input = event.target.value;
-  loadVideos(input);
-});
+//     `;
+//     videosContainer.append(videoCard);
+//   });
+//   hideLoader();
+// };
 
-loadCategoric();
+// document.getElementById("search-input").addEventListener("keyup", (event) => {
+//   const input = event.target.value;
+//   loadVideos(input);
+// });
+
+// loadCategoric();
